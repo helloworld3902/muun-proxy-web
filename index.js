c4a42635e38acb92fa3d40f31b96e0e194e52bee
moved <35048878+innerjoker@users.noreply.github.com>
2019-05-31 13:56:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ http
       if (err) {
         res.writeHead(500, headers);
         return res.end(err.stack || err);
-      }
-      if (req.url == '/') {
-        res.writeHead(301, {
-          'Location': 'https://muun.cf/'
-        });
-        res.end();
       } else {
         res.writeHead(404, headers);
         return res.end('Error 404: file not found.');
